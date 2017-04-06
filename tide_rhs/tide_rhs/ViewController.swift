c056bd42022ffc92fb4c6d2ac56fc946b6d8d475
ELRAF <chad@mastixinc.com>
2017-04-06 18:39:21 -0400

    Created tide application with logo icon.

---
+++ tide_rhs/tide_rhs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tide_rhs
+//
+//  Created by Rafael Boccuni-Godfrey on 4/6/17.
+//  Copyright © 2017 ELRAF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
